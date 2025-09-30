1aceeefa9da94bbcee402ee1478dee8dd7d20646
Mauricio Cuello <mauricio.cuello@key-code.com>
2025-10-01 00:30:56 +0200

    Import and fix params

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
 from finops_analyzer.api.v1.pages import router as pages_v1_router
+from finops_analyzer.api.deps import templates
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -27,10 +28,14 @@ async def create_upload_file(file: UploadFile = File(...)):
 @app.post("/converter")
 async def converter_post(
         request: Request,
-        provider_detection: str, 
         file_upload: UploadFile,
-        provider: str = None
+        provider: str = None,
+        provider_detection: str = None, 
     ):
+    print(f"Request: {request}")
+    url_str = request.url
+    query_str = request.body
+    print(f"Request object: {url_str} - {query_str}")
     filename = file_upload.filename
     content = await file_upload.read()
     print(f"Received file: {filename} with size {len(content)} bytes")
