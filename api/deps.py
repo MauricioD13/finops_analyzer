712b2467d348cc4814647b74a8020bd84c7a084f
Mauricio Cuello <mauricio.cuello@key-code.com>
2025-10-07 20:49:36 +0200

    Focus converter service

--- api/deps.py
+++ api/deps.py
@@ -1,4 +1,7 @@
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-
+from finops_analyzer.focus_converter import FocusConverterService
 templates = Jinja2Templates(directory="templates")
+
+def get_focus_converter_service():
+    yield FocusConverterService() # REVISAR
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.responses import RedirectResponse, JSONResponse
 from finops_analyzer.api.v1.pages import router as pages_v1_router
 from finops_analyzer.api.deps import templates
 from finops_analyzer import schemas
+from finops_analyzer.focus_converter import FocusConverterService
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -15,16 +16,6 @@ MAX_FILE_SIZE = 100 * 1024 * 1024  # 50 MB
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/pages/")
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
-
 
 @app.post("/converter")
 async def converter_post(
