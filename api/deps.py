56b6db0700f035962a4411f6988432133d73a76c
Mauricio Cuello <mauricio.cuello@key-code.com>
2025-10-15 10:38:55 +0200

    Singleton for service

--- api/deps.py
+++ api/deps.py
@@ -1,7 +1,8 @@
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from finops_analyzer.focus_converter import FocusConverterService
+
 templates = Jinja2Templates(directory="templates")
 
-def get_focus_converter_service():
-    yield FocusConverterService() # REVISAR
+def get_focus_converter_service() -> FocusConverterService:
+    return FocusConverterService("focus-converter")
