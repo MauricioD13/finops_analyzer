57f325d6dd7802239ae1f11116dbec47cec932c7
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-10-15 22:58:07 +0200

    Fix names and values

--- schemas.py
+++ schemas.py
@@ -3,8 +3,8 @@ from typing import List, Optional, Literal
 
 
 class ProcessFileRequest(BaseModel):
-    provider: Optional[Literal["aws","azure","oracle","gcp"]] = Field(None, description="The cloud provider (e.g., AWS, GCP, Azure)")
-    provider_detection: Literal["manual","auto"] = Field(..., description="Method for provider detection (e.g., auto, manual)")
+    provider: Optional[Literal["aws","azure","oracle","gcp", None]] = Field(None, description="The cloud provider (e.g., AWS, GCP, Azure)")
+    provider_detection: Literal["manual","automatic"] = Field(..., description="Method for provider detection (e.g., auto, manual)")
     file_content: bytes = Field(..., description="Content of the uploaded file")
     file_path: str = Field(max_length=2000, description="Temp file path")
     file_name: str = Field(max_length=2000, description="File name")
