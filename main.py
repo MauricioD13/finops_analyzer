54e619e303795bb3c9f8fbcce832334204d0e7ad
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-10-20 23:41:08 +0200

    Temp file creation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import tempfile
 from typing import Union, Annotated
-
 from fastapi import FastAPI, UploadFile, File, Request, Form, Depends
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, JSONResponse
+import os
+import subprocess
+
 from finops_analyzer.api.v1.pages import router as pages_v1_router
 from finops_analyzer.api.deps import templates
 from finops_analyzer import schemas
@@ -14,9 +17,19 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.include_router(pages_v1_router, tags=["pages"])
 MAX_FILE_SIZE = 100 * 1024 * 1024  # 50 MB
 
+
 @app.get("/")
 def read_root():
     return RedirectResponse(url="/pages/")
+
+@app.get("/debug/user")
+def debug_user():
+    return {
+        "process_user": os.getenv("USER"),
+        "process_uid": os.getuid(),
+        "process_gid": os.getgid(),
+        "groups": subprocess.run(['groups'], capture_output=True, text=True).stdout
+    }
 
 @app.post("/converter")
 async def converter_post(
@@ -43,16 +56,30 @@ async def converter_post(
     # Reconstruir el contenido completo
     contents = b''.join(chunks)
     print(f"Path: {file_upload.file.name}, Name: {file_upload.filename}")
+        
+    
     if file_upload.file.name is None:
-        # PENDIENTE: Caso en donde el archivo es pequeño y no escribe en un archivo, sino que lo deja en memoria
-        pass
-    file_obj = schemas.ProcessFileRequest(
-        file_content=contents,
-        provider_detection=provider_detection,
-        file_path=file_upload.file.name,
-        file_name=file_upload.filename
-        )
-    result = focus_converter_service.convert_file(file_obj)
+        # Create a named temporary file
+        with tempfile.NamedTemporaryFile(mode='w+t', delete=True, suffix=".txt", dir="/INp") as named_temp_file:
+            named_temp_file.write("More temporary content.")
+            named_temp_file.seek(0)
+            file_path = named_temp_file.name
+            file_obj = schemas.ProcessFileRequest(
+                file_content=contents,
+                provider_detection=provider_detection,
+                file_path=file_path,
+                file_name=file_upload.filename
+            )
+            result = focus_converter_service.convert_file(file_obj)
+            
+    else:
+        file_obj = schemas.ProcessFileRequest(
+                file_content=contents,
+                provider_detection=provider_detection,
+                file_path=file_upload.file.name,
+                file_name=file_upload.filename
+            )
+        result = focus_converter_service.convert_file(file_obj)
     if result:
         return {
             "filename": file_upload.filename,
