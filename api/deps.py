6ea6543308d1668bdd43b8d6fc2aff76485757f7
Mauricio Cuello <mauricio.cuello@gmail.com>
2025-11-14 12:18:56 +0100

    get service

--- api/deps.py
+++ api/deps.py
@@ -1,8 +1,11 @@
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from finops_analyzer.focus_converter.service import FocusConverterService
+from finops_analyzer.focus_converter.service import FocusConverterService, FileDeleterService
 
 templates = Jinja2Templates(directory="templates")
 
 def get_focus_converter_service() -> FocusConverterService:
     return FocusConverterService("focus-converter")
+
+def get_file_deleter_service() -> FileDeleterService:
+    return FileDeleterService()
