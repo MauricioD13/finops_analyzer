45460d2653bfa95043c963d1fc7506249119aafd
Mauricio Cuello <mdavidcuello@gmail.com>
2025-11-25 11:52:26 +0100

    Fix get service

--- api/deps.py
+++ api/deps.py
@@ -1,11 +1,15 @@
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from finops_analyzer.focus_converter.service import FocusConverterService, FileDeleterService
+from converter_services.service import FocusConverterService, FileDeleterService
+from main import UPLOAD_DIR, DOWNLOAD_DIR
 
 templates = Jinja2Templates(directory="templates")
 
 def get_focus_converter_service() -> FocusConverterService:
-    return FocusConverterService("focus-converter")
+    return FocusConverterService(
+        input_dir=UPLOAD_DIR,
+        output_dir=DOWNLOAD_DIR
+    )
 
 def get_file_deleter_service() -> FileDeleterService:
     return FileDeleterService()
